0fb76f14e4834fc7d1ba60f8f4bd08c48db6b973
Rohan Patel <patelrohan@users.noreply.github.com>
2023-08-08 08:32:18 -0400

    ContentView Q&A setup
    Utilized VStack, HStack, ZStack to create a view for Quiz app

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            mainColor.ignoresSafeArea()
+            VStack{
+                Text("1/10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                
+                HStack{
+                    Button(action: {
+                        print("Tapped on Ant")
+                    }, label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 2.5)
+                    })
+                    
+                    Button(action: {
+                        print("Tapped on Beetle")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 2.5)
+                    })
+                    
+                    Button(action: {
+                        print("Tapped on Moth")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 2.5)
+                    })
+                    
+                    Button(action: {
+                        print("Tapped on Fly")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 2.5)
+                    })
+                    
+                    
+                }
+            }
+            
         }
-        .padding()
+        .foregroundColor(.white)
     }
 }
 

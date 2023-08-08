0c4e156f1b6ed9c218985e9d5043b70bade37b38
Rohan Patel <patelrohan@users.noreply.github.com>
2023-08-08 08:13:59 -0400

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Rohan on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
